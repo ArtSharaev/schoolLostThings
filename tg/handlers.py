67b1af0c02f8f38bcb538ef3f694466e68f23805
ArtSharaev <sharaev_00@inbox.ru>
2022-11-09 09:04:52 +0300

    photo saving technology

--- tg/handlers.py
+++ tg/handlers.py
@@ -3,6 +3,8 @@ from aiogram.types import ContentType
 from tg.utils import States
 from tg.messages import MESSAGES, BUILDINGS_LIST
 from tg.keyboards import *
+import os
+import os.path
 
 from main import dp, bot
 
@@ -19,8 +21,7 @@ async def process_help_command(msg: types.Message):
 
 @dp.message_handler(content_types=ContentType.PHOTO)
 async def process_get_photo_command(msg: types.Message):
-    file_info = await bot.get_file(msg.photo[-1].file_id)
-    await msg.photo[-1].download(file_info.file_path)
+    await msg.photo[-1].download("photos/base_name.jpg")
     state = dp.current_state(user=msg.from_user.id)
     await bot.send_message(msg.from_user.id, MESSAGES["ask_building"],
                            reply_markup=choose_building_markup)
@@ -53,5 +54,13 @@ async def process_get_room_command(msg: types.Message):
             await bot.send_message(msg.from_user.id, MESSAGES["room_error"])
             await state.set_state(States.all()[1])
         else:
+            state_data = await state.get_data()
+            building = state_data["building"]
+            k = 0
+            new_filename = f"photos/{building}--{number}--({k}).jpg"
+            while os.path.exists(new_filename):
+                k += 1
+                new_filename = f"photos/{building}--{number}--({k}).jpg"
+            os.rename("photos/base_name.jpg", new_filename)
             await bot.send_message(msg.from_user.id, MESSAGES["finish"])
             await state.finish()
