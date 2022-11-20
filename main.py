899d5be9aea35ba0b09ae334ecad5dd0e95b725c
ArtSharaev <sharaev_00@inbox.ru>
2022-11-20 20:34:03 +0300

    ip change

--- main.py
+++ main.py
@@ -26,5 +26,5 @@ async def shutdown(dispatcher: Dispatcher):
 
 
 if __name__ == '__main__':
-    app.run(host="192.168.43.194", port=5000)
+    app.run(host="0.0.0.0", port=5000)
     executor.start_polling(dp)
