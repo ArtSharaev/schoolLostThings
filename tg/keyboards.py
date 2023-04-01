8a72c06afbbe6404c0464b02ac9eb0c7e94d2011
markizzkiselev <markizzkiselev@yandex.ru>
2023-04-01 23:00:53 +0300

    bot messages upd

--- tg/keyboards.py
+++ tg/keyboards.py
@@ -4,11 +4,11 @@ from aiogram.types import ReplyKeyboardRemove, \
 
 choose_building_markup = ReplyKeyboardMarkup(resize_keyboard=True,
                                              one_time_keyboard=True)
-btn1 = KeyboardButton("Барклая, дом 15, к. 3 (𝟭𝟰𝟵𝟳)")
-btn2 = KeyboardButton("Новозаводская, дом 19 (𝟳𝟯𝟳)")
+btn1 = KeyboardButton("Барклая, дом 15, к. 3")
+btn2 = KeyboardButton("Новозаводская, дом 19")
 choose_building_markup.row(btn1, btn2)
-btn3 = KeyboardButton("Большая Филевская, дом 15 (𝟳𝟐)")
+btn3 = KeyboardButton("Большая Филевская, дом 15")
 choose_building_markup.row(btn3)
-btn4 = KeyboardButton("Филевский б-р, дом 13 к. 2 (𝟭𝟭𝟭𝟰)")
-btn5 = KeyboardButton("Филевский б-р, дом 3 к. 2 (𝟭𝟭𝟭𝟰)")
+btn4 = KeyboardButton("Филевский б-р, дом 13, к. 2")
+btn5 = KeyboardButton("Филевский б-р, дом 3, к. 2")
 choose_building_markup.row(btn4, btn5)
