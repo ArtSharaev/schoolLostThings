faed1e58045894354c1d8a941f3c05d551e5e2b3
markizzkiselev <markizzkiselev@yandex.ru>
2023-04-01 22:23:54 +0300

    Обновление Максима Киселёва

--- flask_app/views.py
+++ flask_app/views.py
@@ -11,33 +11,38 @@ def index():
 @app.route('/B15-3', methods=['POST', 'GET'])
 def b15_3():
     return render_template('building.html',
-                           building_name='Барклая, дом 15, к. 3 (𝟭𝟰𝟵𝟳)',
+                           building_name='Барклая, дом 15, к. 3',
+                           building_num='1497',
                            photos=get_files("photos/B15-3"))
 
 
 @app.route('/N19', methods=['POST', 'GET'])
 def n19():
     return render_template('building.html',
-                           building_name='Новозаводская, дом 19 (𝟳𝟯𝟳)',
+                           building_name='Новозаводская, дом 19',
+                           building_num='737',
                            photos=get_files("photos/N19"))
 
 
 @app.route('/BF15', methods=['POST', 'GET'])
 def bf15():
     return render_template('building.html',
-                           building_name='Большая Филевская, дом 15 (𝟳𝟐)',
+                           building_name='Большая Филёвская, дом 15',
+                           building_num='72',
                            photos=get_files("photos/BF15"))
 
 
 @app.route('/FB13-2', methods=['POST', 'GET'])
 def fb13_2():
     return render_template('building.html',
-                           building_name='Филевский б-р, дом 13 к. 2 (𝟭𝟭𝟭𝟰)',
+                           building_name='Филёвский б-р, дом 13, к. 2',
+                           building_num='1114',
                            photos=get_files("photos/FB13-2"))
 
 
 @app.route('/FB3-2', methods=['POST', 'GET'])
 def fb3_2():
     return render_template('building.html',
-                           building_name='Филевский б-р, дом 3 к. 2 (𝟭𝟭𝟭𝟰)',
+                           building_name='Филёвский б-р, дом 3, к. 2',
+                           building_num='1114',
                            photos=get_files("photos/FB3-2"))
