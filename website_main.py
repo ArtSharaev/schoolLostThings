74e635b9247dd40d9e7d8e7d2a3cd55ed888f2e9
art.sharaev <sharaev_00@inbox.ru>
2023-04-11 20:30:46 +0300

    padding changes

--- website_main.py
+++ website_main.py
@@ -8,4 +8,4 @@ logging.basicConfig(format=u'%(filename)+13s [ LINE:%(lineno)-4s]'
                     filemode='w')
 
 if __name__ == '__main__':
-    application.run(host='0.0.0.0')
+    application.run(host='0.0.0.0', port="8080")
